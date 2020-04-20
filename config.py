9d36bd26bb75cbe7f4cb9ad0e824fdee8a3c7cfc
oleslav <oleslav.boychuk@gmail.com>
2020-04-20 22:14:11 +0300

    add config
    config .py for constans

---
+++ config.py
@@ -0,0 +1,2 @@
+
+token = "1086478120:AAENzfoluFzk4OYpFPsoJ-sO8ib_Lxp3crI"
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-# -*- coding: utf-8 -*-
-#import config
 import telebot
+import config
 
-token = "1086478120:AAENzfoluFzk4OYpFPsoJ-sO8ib_Lxp3crI"
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
