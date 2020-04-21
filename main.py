bcd00286a349638e2b135784cecc39091c0a9b1d
oleslav <oleslav.boychuk@gmail.com>
2020-04-21 12:43:07 +0300

    .

--- main.py
+++ main.py
@@ -3,40 +3,78 @@ from telebot import types
 from telebot import types
 import shelve
 import config
-import Controller
 
-bot = telebot.TeleBot("1086478120:AAENzfoluFzk4OYpFPsoJ-sO8ib_Lxp3crI")
+bot = telebot.TeleBot(config.token)
+
+
+
+import  mysql.connector;
+
+
+database = mysql.connector.connect(user='root', password='root', 
+                              host='localhost', 
+                              database='newschema')
 
 
 @bot.message_handler(commands=['start'])
 def start(mess):
     chat_id = mess.chat.id
     markup = types.ReplyKeyboardMarkup()
-    btn_a = types.KeyboardButton('Push')
-    btn_b = types.KeyboardButton('Pull')
-    markup.add(btn_a, btn_b)
+    btn_a = types.KeyboardButton('Show Subject')
+    markup.add(btn_a)
     bot.send_message(chat_id, 'Привет', reply_markup = markup)
 
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-    if message.text == "Push":
+    if message.text == "Show Subject":
+        if database.is_connected():
+            cursor = database.cursor()
+            cursor.execute("select name, subject_id from subjects;")
+            subjects = cursor.fetchall();
+            keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
+            for tmp in subjects:
+                keyboard.add(types.InlineKeyboardButton(text=str(tmp[0]), callback_data= 's' + str(tmp[1])))
+            bot.send_message(message.from_user.id, text = "Subjects:" , reply_markup = keyboard)
+                
+            
 
-        bot.send_message(message.from_user.id, "Привет")
-        keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
-        key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes'); #кнопка «Да»
-        keyboard.add(key_yes); #добавляем кнопку в клавиатуру
-        key_no= types.InlineKeyboardButton(text='Нет', callback_data='no');
-        keyboard.add(key_no);
-        question = 'Да или Нет?';
-        bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-    elif message.text == "Pull":
-        bot.send_message(message.from_user.id, "НИЧЕГО")
-        bot.send_message(message.from_user.id, "Push")
-    if message.text == "Pull":
-        bot.send_message(message.from_user.id, "Pull")
-    elif message.text == "/hint":
-        bot.send_message(message.from_user.id, "hint")
+
+
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    print(call.data)
+    if database.is_connected():
+
+        if call.data[0] == 's':
+            cursor = database.cursor()
+            cursor.execute("select title, sSubject_id from sSubject where subject_id = " + str(call.data[1:]) + ";")
+            subjects = cursor.fetchall();
+            keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
+            for tmp in subjects:
+                keyboard.add(types.InlineKeyboardButton(text=str(tmp[0]), callback_data='t' + str(tmp[1])))
+            bot.send_message(call.from_user.id, text = "SubSubject:" , reply_markup = keyboard)
+        elif call.data[0] == 't':
+            cursor = database.cursor()
+            cursor.execute("select title, topic_id from topic where ssubject_id = " + str(call.data[1:]) + ";")
+            subjects = cursor.fetchall();
+            keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
+            for tmp in subjects:
+                keyboard.add(types.InlineKeyboardButton(text=str(tmp[0]), callback_data='u' + str(tmp[1])))
+            bot.send_message(call.from_user.id, text = "Topic:" , reply_markup = keyboard)
+        elif call.data[0] == 'u':
+            cursor = database.cursor()
+            cursor.execute("select url from links where topic_id = " + str(call.data[1:]) + " order by rating;")
+            subjects = cursor.fetchall();
+            keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
+            for tmp in subjects:
+                keyboard.add(types.InlineKeyboardButton(text='Lection', url = tmp[0]))
+            bot.send_message(call.from_user.id, text = "Лекции" , reply_markup = keyboard)
+    
+      
+
 
 
 
