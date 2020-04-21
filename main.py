50701262d52a311f48930f6c8cbaafd2d2c670bb
Oleslav <oleslav.boychuk@gmail.com>
2020-04-21 21:40:15 +0300

    Bot change message

--- main.py
+++ main.py
@@ -28,7 +28,9 @@ def handle_text(message):
             keyboard = types.InlineKeyboardMarkup();  # наша клавиатура
             for tmp in subjects:
                 keyboard.add(types.InlineKeyboardButton(text=str(tmp[0]), callback_data='s' + str(tmp[1])))
-            bot.send_message(message.from_user.id, text="Subjects:", reply_markup=keyboard)
+            global sended_message
+            sended_message = bot.send_message(message.from_user.id, text="Subjects:", reply_markup=keyboard)
+            print(sended_message)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -42,7 +44,8 @@ def callback_inline(call):
             keyboard = types.InlineKeyboardMarkup();  # наша клавиатура
             for tmp in subjects:
                 keyboard.add(types.InlineKeyboardButton(text=str(tmp[0]), callback_data='t' + str(tmp[1])))
-            bot.send_message(call.from_user.id, text="SubSubject:", reply_markup=keyboard)
+            # bot.send_message(call.from_user.id, text="SubSubject:", reply_markup=keyboard)
+            bot.edit_message_text(chat_id=call.from_user.id, message_id=sended_message.message_id, text="SubSubject:",reply_markup=keyboard)
         elif call.data[0] == 't':
             cursor = database.cursor()
             cursor.execute("select title, topic_id from topic where ssubject_id = " + str(call.data[1:]) + ";")
@@ -50,7 +53,8 @@ def callback_inline(call):
             keyboard = types.InlineKeyboardMarkup();  # наша клавиатура
             for tmp in subjects:
                 keyboard.add(types.InlineKeyboardButton(text=str(tmp[0]), callback_data='u' + str(tmp[1])))
-            bot.send_message(call.from_user.id, text="Topic:", reply_markup=keyboard)
+            # bot.send_message(call.from_user.id, text="Topic:", reply_markup=keyboard)
+            bot.edit_message_text(chat_id=call.from_user.id, message_id=sended_message.message_id, text="Topic:", reply_markup=keyboard)
         elif call.data[0] == 'u':
             cursor = database.cursor()
             cursor.execute("select url, title from links where topic_id = " + str(call.data[1:]) + " order by rating;")
@@ -58,7 +62,8 @@ def callback_inline(call):
             keyboard = types.InlineKeyboardMarkup();  # наша клавиатура
             for tmp in subjects:
                 keyboard.add(types.InlineKeyboardButton(text=tmp[1], url=tmp[0]))
-            bot.send_message(call.from_user.id, text="Посилання", reply_markup=keyboard)
+            # bot.send_message(call.from_user.id, text="Посилання", reply_markup=keyboard)
+            bot.edit_message_text(chat_id=call.from_user.id, message_id=sended_message.message_id, text="Посилання", reply_markup=keyboard)
 
 
 if __name__ == '__main__':
